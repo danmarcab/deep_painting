b627a542940070487557d708104c225ca58ffc34
Daniel Marin <danmarcab@gmail.com>
2017-03-04 20:25:02 +0000

    add studio app

---
+++ apps/studio/lib/studio.ex
@@ -0,0 +1,18 @@
+defmodule Studio do
+  @moduledoc """
+  Documentation for Studio.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Studio.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
